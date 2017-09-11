ce1b59b3b301b007b62b9ea5f2ee26a68938d70c
Tony Cannistra <tony.cannistra@gmail.com>
2017-09-11 10:08:30 -0700

    started future

---
+++ r/future.R
@@ -0,0 +1,10 @@
+## Physiology in Species Distribution Models
+## Tony Cannistra & Lauren Buckley, 2017
+## ~ Future Projection and Evaluation helper functions. 
+suppressPackageStartupMessages({
+  library(raster)
+})
+
+getClusters <- function(predictionRaster){
+  
+}
